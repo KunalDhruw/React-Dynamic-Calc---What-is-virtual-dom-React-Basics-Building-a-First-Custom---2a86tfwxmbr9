63140c23b68aac41907100f8be4c5935dd9964b4
KunalDhruw <108332747+KunalDhruw@users.noreply.github.com>
2023-01-15 17:29:37 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React,{useState,useEffect} from 'react'
+import '../styles/App.css';
+const App = () => {
+
+  return (
+    <div id="main">
+      <input id='input1' onChange={}/>
+       +
+      <input id='input2' onChange={}/> 
+      
+      <p id='result'></p>
+    </div>
+  )
+}
+
+
+export default App;
