01c3c6d32017bcc90fd3e4e3a5b1ccc87a6ce242
KunalDhruw <108332747+KunalDhruw@users.noreply.github.com>
2023-01-15 17:35:47 +0530

    Updated.

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,21 @@
-import React,{useState,useEffect} from 'react'
-import '../styles/App.css';
-const App = () => {
+import React, { useState, useEffect } from "react";
+import "../styles/App.css";
+const handle = () => {
+  let a = document.getElementById("input1").value;
+  let b = document.getElementById("input2").value;
+  let c = parseInt(a) + parseInt(b);
+  return (document.getElementById("result").innerHTML = c);
+};
 
+const App = () => {
   return (
     <div id="main">
-      <input id='input1' onChange={}/>
-       +
-      <input id='input2' onChange={}/> 
-      
-      <p id='result'></p>
+      <input id="input1" onChange={handle} />
+      +
+      <input id="input2" onChange={handle} />
+      <p id="result">0</p>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
